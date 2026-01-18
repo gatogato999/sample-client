450c4aeee128966d700340d75bf96c7424e40a80
gatogato999 <ggaattoo38@gmail.com>
2026-01-18 10:41:21 +0200

    Revert "modified : /users now is protected by jwt"
    This reverts commit e8029bd1f0d112c830ba9f6e21700eba52a93f71.
    
    back to old files

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 type User struct {
@@ -14,7 +13,7 @@ type User struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
-	Password  string `json:"hashed_password"`
+	Password  string `json:"password"`
 	Phone     string `json:"phone"`
 	Age       int    `json:"age"`
 	Job       string `json:"job"`
@@ -23,51 +22,50 @@ type User struct {
 func main() {
 	var err error
 	var url string
-	var jwtToken string
 
 	// NOTE: base route /
-	url = "http://localhost:8080/"
-	err = pingUrl(url)
-	checkError(err)
-
-	// NOTE: insert a user to the database
-	url = "http://localhost:8080/register"
-	user := User{
-		FirstName: "user1",
-		LastName:  "user1",
-		Email:     "user1@gmail.com",
-		Password:  "passpass",
-		Phone:     "123456789",
-		Age:       29,
-		Job:       "user1job",
-	}
-	err = insertUser(url, user)
-	checkError(err)
-
-	// NOTE: loggin : /auth
-	url = "http://localhost:8080/auth"
-	err = logginUser(url, user.Email, user.Password)
-	checkError(err)
+	// url = "http://localhost:8080/"
+	// err = pingUrl(url)
+	// checkError(err)
 
 	// NOTE: get users route /users
+	// url = "http://localhost:8080/users"
+	// err = getAllUsers(url, "mo@gmail.com")
+	// checkError(err)
 
-	url = "http://localhost:8080/users"
-	jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsImV4cCI6MTc2ODUwNzA2NywiaWF0IjoxNzY4NTA2MTY3LCJzdWIiOiJ1c2VyMUBnbWFpbC5jb20ifQ.EkMrOQkpttbel7kj-pVBM7d6T8LIEf8FLIlP2X8oBmw"
-	err = getAllUsers(url, "mo@gmail.com", jwtToken)
-	checkError(err)
+	// NOTE:  insert a user to the database
+	// url = "http://localhost:8080/register"
+	// user := User{
+	// 	FirstName: "ftestData",
+	// 	LastName:  "ltestData",
+	// 	Email:     "momto@gmal.com",
+	// 	Password:  "pass",
+	// 	Phone:     "12345678",
+	// 	Age:       89,
+	// 	Job:       "testData",
+	// }
+	// err = insertUser(url, user)
+	// checkError(err)
+
+	// NOTE: loggin : /auth
+	// url = "http://localhost:8080/auth"
+	// err = logginUser(url, user.Email, user.Password)
+	// checkError(err)
 
 	// NOTE: get search for user : /query
 	url = "http://localhost:8080/query"
-	jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsImV4cCI6MTc2ODUwNzA2NywiaWF0IjoxNzY4NTA2MTY3LCJzdWIiOiJ1c2VyMUBnbWFpbC5jb20ifQ.EkMrOQkpttbel7kj-pVBM7d6T8LIEf8FLIlP2X8oBmw"
-	err = searchUserByEmail(url, "mo@gmail.com", jwtToken)
+	jwtToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vbXRvQGdtYWwuY29tIiwiZXhwIjoxNzY4NDA2NDg0LCJpYXQiOjE3Njg0MDU1ODQsInN1YiI6Im1vbXRvQGdtYWwuY29tIn0.9QHdpDY4kOE8jER9DtDwxSI1VYmQqKeTkYMbOsDBLqg"
+	err = searchUserByEmail(url, "kmomto@gmal.com", jwtToken)
 	checkError(err)
 }
 
-func insertUser(url string, user User) error {
-	reqBody, err := json.Marshal(user)
+func searchUserByEmail(url, email, jwtToken string) error {
+	token := "Bearer " + jwtToken
+	url = url + fmt.Sprint("/", email)
+	req, err := http.NewRequest("POST", url, nil)
 	checkError(err)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
-	checkError(err)
+
+	req.Header.Add("Authorization", token)
 	req.Header.Set("Accept", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
@@ -94,16 +92,11 @@ func logginUser(url, email, password string) error {
 	return nil
 }
 
-func getAllUsers(url, email, jwtToken string) error {
-	userInputs := map[string]string{"email": email}
-	token := "Bearer " + jwtToken
-
-	reqBody, err := json.Marshal(userInputs)
+func insertUser(url string, user User) error {
+	reqBody, err := json.Marshal(user)
 	checkError(err)
-
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
 	checkError(err)
-	req.Header.Add("Authorization", token)
 	req.Header.Set("Accept", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
@@ -114,14 +107,14 @@ func getAllUsers(url, email, jwtToken string) error {
 	return nil
 }
 
-func searchUserByEmail(url, email, jwtToken string) error {
-	token := "Bearer " + jwtToken
+func getAllUsers(url, email string) error {
+	userInputs := map[string]string{"email": email}
 
-	url = url + fmt.Sprint("/", email)
-	req, err := http.NewRequest("POST", url, nil)
+	reqBody, err := json.Marshal(userInputs)
 	checkError(err)
 
-	req.Header.Add("Authorization", token)
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(reqBody))
+	checkError(err)
 	req.Header.Set("Accept", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
@@ -148,20 +141,19 @@ func pingUrl(url string) error {
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 }
 
 func getResponse(res *http.Response) {
 	defer res.Body.Close()
 
-	fmt.Println("\n-------------------------\nResponse status:", res.Status)
+	fmt.Println("Response status:", res.Status)
 
 	var data any
 	err := json.NewDecoder(res.Body).Decode(&data)
 	checkError(err)
 
-	b, err := json.Marshal(data)
+	b, err := json.MarshalIndent(data, "", "")
 	checkError(err)
 	fmt.Println("\n", string(b))
 }
